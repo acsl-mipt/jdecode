a35a2a0c4dd1ed8839425e5ed7d9d37d7f836ce4
Artem Shein <metadeus@aisys.ru>
2015-11-02 14:15:15 +0300

    Fix Maven build

--- c-sources-generator/src/main/scala/ru/mipt/acsl/decode/c/generator/CDecodeSourcesGenerator.scala
+++ c-sources-generator/src/main/scala/ru/mipt/acsl/decode/c/generator/CDecodeSourcesGenerator.scala
@@ -1,17 +1,22 @@
 package ru.mipt.acsl.decode.c.generator
 
+import java.io.File
 import java.util.Optional
 
+import com.typesafe.scalalogging.LazyLogging
+import ru.mipt.acsl.decode.model.domain.DecodeRegistry
 import ru.mipt.acsl.generation.Generator
 
-trait CDecodeGeneratorConfiguration
+class CDecodeGeneratorConfiguration(val outputDir: File, val registry: DecodeRegistry)
 {
 
 }
 
-class CDecodeSourcesGenerator(val config: Option[CDecodeGeneratorConfiguration]) extends Generator[CDecodeGeneratorConfiguration]
+class CDecodeSourcesGenerator(val config: Option[CDecodeGeneratorConfiguration] = None) extends Generator[CDecodeGeneratorConfiguration] with LazyLogging
 {
-  override def generate(): Unit = ???
+  override def generate(): Unit = {
 
-  override def getConfiguration: Optional[CDecodeGeneratorConfiguration] = config.to[Optional]
+  }
+
+  override def getConfiguration: Optional[CDecodeGeneratorConfiguration] = Optional.ofNullable(config.orNull)
 }
