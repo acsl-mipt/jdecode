a65d67ca70cae9e3e724fafda6b4f416fa7b66e5
Artem Shein <metadeus@aisys.ru>
2016-02-12 14:11:01 +0300

    Plugin parser fix

---
+++ idea-plugin/gen/ru/mipt/acsl/decode/parser/psi/impl/DecodeEntityIdImpl.java
@@ -0,0 +1,31 @@
+// This is a generated file. Not intended for manual editing.
+package ru.mipt.acsl.decode.parser.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static ru.mipt.acsl.decode.parser.psi.DecodeTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import ru.mipt.acsl.decode.parser.psi.*;
+
+public class DecodeEntityIdImpl extends ASTWrapperPsiElement implements DecodeEntityId {
+
+  public DecodeEntityIdImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof DecodeVisitor) ((DecodeVisitor)visitor).visitEntityId(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getNonNegativeNumber() {
+    return findNotNullChildByType(NON_NEGATIVE_NUMBER);
+  }
+
+}
