2c7a5cc217f7068c9c1d8f0eece4826904b6f10b
Artem Shein <metadeus@aisys.ru>
2016-02-19 20:50:15 +0300

    Fix build

--- parser/src/main/scala/ru/mipt/acsl/decode/parser/scripts.scala
+++
@@ -1,9 +0,0 @@
-package ru.mipt.acsl.decode.parser
-
-import scala.tools.refactoring.util.CompilerProvider
-
-object ScriptParser extends CompilerProvider {
-  def parse(str: String) = {
-    treeFrom(str)
-  }
-}
