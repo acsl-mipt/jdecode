e4b3f57ec9833d379b4ab29082c6c66b364d301a
Artem Shein <sc-github@aisys.ru>
2016-03-30 02:09:42 +0300

    Add missing files

---
+++ parser/gen/ru/mipt/acsl/decode/parser/psi/impl/DecodeParameterPathElementImpl.java
@@ -0,0 +1,41 @@
+// This is a generated file. Not intended for manual editing.
+package ru.mipt.acsl.decode.parser.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static ru.mipt.acsl.decode.parser.psi.DecodeTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import ru.mipt.acsl.decode.parser.psi.*;
+
+public class DecodeParameterPathElementImpl extends ASTWrapperPsiElement implements DecodeParameterPathElement {
+
+  public DecodeParameterPathElementImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull DecodeVisitor visitor) {
+    visitor.visitParameterPathElement(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof DecodeVisitor) accept((DecodeVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public DecodeElementNameRule getElementNameRule() {
+    return findChildByClass(DecodeElementNameRule.class);
+  }
+
+  @Override
+  @Nullable
+  public DecodeRangeDecl getRangeDecl() {
+    return findChildByClass(DecodeRangeDecl.class);
+  }
+
+}
