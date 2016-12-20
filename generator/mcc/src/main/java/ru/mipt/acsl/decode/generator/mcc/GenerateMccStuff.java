cadc53f1efa083f4c0af142d58f36af0d4f0d744
Карташев Сергей <sergei.kartashev@yandex.ru>
2016-12-20 11:27:34 +0300

    mavlink-mcc generator

--- generator/mcc/src/main/java/ru/mipt/acsl/decode/generator/mcc/GenerateMccStuff.java
+++ generator/mcc/src/main/java/ru/mipt/acsl/decode/generator/mcc/GenerateMccStuff.java
@@ -16,12 +16,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+import org.apache.commons.cli.*;
+import java.nio.charset.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 /**
  * @author Artem Shein
  */
@@ -29,26 +37,90 @@ public class GenerateMccStuff {
 
     public static final Fqn MAVLINK_COMPONENT_FQN = Fqn.newInstance("mavlink.Common");
 
-    public static final String MAVLINK_SOURCE_RESOURCE = "mavlink/common.xml";
-    public static final List<String> MAVLINK_INCLUDES = new ArrayList<>();
+    public static Options constructPosixOptions()
+    {
+        final Options posixOptions = new Options();
+        posixOptions.addOption("i", "input", true, "common.xml path")
+                    .addOption("o", "output", true, "mcc directory path");
 
-    public static final String MCC_DECODE_SOURCES_PATH = "src/mcc/core/db/db/sources/";
-    public static final String MAVLINK_FILE_PATH = MCC_DECODE_SOURCES_PATH + "mavlink.decode";
-    public static final String MODEL_FILE_PATH = "src/mcc/core/db/db/model.json";
+        return posixOptions;
+    }
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public static List<String> getIncludes(String xmlPath)
+    {
+        List<String> includes = new ArrayList<String>();
+        try {
+            File fXmlFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            String includeNode = doc.getDocumentElement().getElementsByTagName("include").item(0).getTextContent();
+
+            String sourceDir = fXmlFile.getParentFile().getAbsolutePath();
+            File f2 = new File(sourceDir, includeNode);
+
+            includes.add(f2.getAbsolutePath());
+            return includes;
+        }
+        catch(Exception e)
+        {
+            return new ArrayList<String>();
+        }
+    }
 
     public static void main(String[] args) {
+        final CommandLineParser cmdLinePosixParser = new DefaultParser();
+        final Options posixOptions = constructPosixOptions();
+        CommandLine commandLine;
 
+        String MAVLINK_SOURCE_RESOURCE = "mavlink/common.xml";
+
+        String MCC_DECODE_SOURCES_PATH = "src/mcc/core/db/db/sources/";
+        String MAVLINK_FILE_PATH = MCC_DECODE_SOURCES_PATH + "mavlink.decode";
+        String MODEL_FILE_PATH = "src/mcc/core/db/db/model.json";
+
+        try
+        {
+            commandLine = cmdLinePosixParser.parse(posixOptions, args);
+            MAVLINK_SOURCE_RESOURCE = commandLine.getParsedOptionValue("input").toString();
+            String mccPath = commandLine.getParsedOptionValue("output").toString();
+            MCC_DECODE_SOURCES_PATH = mccPath + "/src/mcc/core/db/db/sources/";
+            MAVLINK_FILE_PATH = MCC_DECODE_SOURCES_PATH + "mavlink.decode";
+            MODEL_FILE_PATH = mccPath + "/src/mcc/core/db/db/model.json";
+        }
+        catch (ParseException parseException)  // checked exception
+        {
+            System.err.println(
+                    "Encountered exception while parsing using PosixParser:\n"
+                            + parseException.getMessage() );
+        }
         try {
+            String finalMAVLINK_SOURCE_RESOURCE = MAVLINK_SOURCE_RESOURCE;
+            String finalMCC_DECODE_SOURCES_PATH = MCC_DECODE_SOURCES_PATH;
+            String finalMAVLINK_FILE_PATH = MAVLINK_FILE_PATH;
+
+            List<String> MAVLINK_INCLUDES = getIncludes(finalMAVLINK_SOURCE_RESOURCE);
             new FileOutputStream(new File(MODEL_FILE_PATH)) {{
 
                 ByteArrayOutputStream mavlinkOutput = new ByteArrayOutputStream();
 
                 Map<String, String> filesContents = new HashMap<>();
-                MAVLINK_INCLUDES.forEach(i -> filesContents.put(new File(i).getName(), resourceContents(i)));
+                for(String s : MAVLINK_INCLUDES)
+                {
+                    filesContents.put(new File(s).getName(), readFile(s, StandardCharsets.UTF_8));
+                }
 
                 // Generate Decode sources for Mavlink
                 MavlinkSourceGenerator.apply(MavlinkSourceGeneratorInternalConfig.newInstance(
-                        resourceContents(MAVLINK_SOURCE_RESOURCE),
+                        readFile(finalMAVLINK_SOURCE_RESOURCE, StandardCharsets.UTF_8),
                         MAVLINK_COMPONENT_FQN.copyDropLast().mangledNameString(),
                         MAVLINK_COMPONENT_FQN.last().mangledNameString(),
                         mavlinkOutput, filesContents)).generate();
@@ -58,7 +130,7 @@ public class GenerateMccStuff {
                     try
                     {
                         new FileOutputStream(
-                                new File(MCC_DECODE_SOURCES_PATH + ModelRegistry.sourceName(sourceFileName)))
+                                new File(finalMCC_DECODE_SOURCES_PATH + ModelRegistry.sourceName(sourceFileName)))
                         {{
                             write(ModelRegistry.sourceContents(sourceFileName).getBytes());
                             close();
@@ -78,7 +150,7 @@ public class GenerateMccStuff {
                 contents.add(new String(mavlinkSourceContents, StandardCharsets.UTF_8));
 
                 // Copy Mavlink sources to MCC
-                File mavlinkSource = new File(MAVLINK_FILE_PATH);
+                File mavlinkSource = new File(finalMAVLINK_FILE_PATH);
                 mavlinkSource.getParentFile().mkdirs();
                 new FileOutputStream(mavlinkSource) {{
                     write(mavlinkSourceContents);
